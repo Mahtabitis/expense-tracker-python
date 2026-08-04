3d724f96067764e72ba58deb638132dca00d9d09
MahtabRamezani <MahtaabRamezanii@gmail.com>
2026-08-05 02:48:56 +0330

    Implement expense table and add expense modal

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return render_template("index.html")
 
+    with open("expenses.json", "r") as file:
+        expenses = json.load(file)
+
+    total = 0
+    for expense in expenses:
+        total += expense["amount"]
+
+    return render_template("index.html", expenses=expenses, total=total)
 
 if __name__ == "__main__":
     app.run(debug=True)
