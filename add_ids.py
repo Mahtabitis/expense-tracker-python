82a1c54c7d9cac935262e49af381e245c2d4fd3f
MahtabRamezani <MahtaabRamezanii@gmail.com>
2026-09-26 14:26:15 +0330

    Remove temporary ID migration script

--- add_ids.py
+++
@@ -1,9 +0,0 @@
-import json
-with open("expenses.json", "r") as file:
-    expenses = json.load(file)
-
-for index, expense in enumerate(expenses, start=1):
-    expense["id"] = index
-
-with open("expenses.json", "w") as file:
-    json.dump(expenses, file, indent=4)
